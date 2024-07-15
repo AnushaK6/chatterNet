96eba08ee7feac93329d4af90f51fe14b56dc998
Anusha <anusha.k1300@gmail.com>
2024-07-15 12:25:40 +0530

    Change link

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const socket = io('ws://localhost:3500')
+const socket = io('https://chatternet-vhmq.onrender.com')
 
 const msgInput = document.querySelector('#msg')
 const activity = document.querySelector('.activity')
